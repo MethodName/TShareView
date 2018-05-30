597eb73fff4d31c86a757853dc35817c3d541ddc
MethodName <methodname@qq.com>
2018-05-30 18:15:46 +0800

    一个共享的组件

---
+++ TShareView/Classes/TMMShareView.h
@@ -0,0 +1,13 @@
+//
+//  ShareView.h
+//  test
+//
+//  Created by 唐明明 on 2018/5/30.
+//  Copyright © 2018年 Methodname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMMShareView : UIView
+
+@end
---
+++ TShareView/Classes/TMMShareView.m
@@ -0,0 +1,21 @@
+//
+//  ShareView.m
+//  test
+//
+//  Created by 唐明明 on 2018/5/30.
+//  Copyright © 2018年 Methodname. All rights reserved.
+//
+
+#import "TMMShareView.h"
+
+@implementation TMMShareView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
